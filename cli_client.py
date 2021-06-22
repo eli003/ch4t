be861299307612c4b4ab3b3d6211a33c0db94623
atoby <tobi.koenig1@gmail.com>
2021-06-22 14:44:08 +0200

    cli_client: first successful local test using ncat

--- cli_client.py
+++ cli_client.py
@@ -1,1 +1,14 @@
-print("hello")
+#file: cli_client.py
+#repository: eli003
+
+import socket
+
+host = "127.0.0.1"
+port = 64001
+
+s=socket.create_connection(("localhost",port))
+
+s.send("Hallo Tobias König".encode("cp850"))   # write data
+bytes=s.recv(1024)          # receive data
+print("Response from server:",bytes.decode("cp850"))      # decode and output received data
+s.close()                   # Close the socket when done

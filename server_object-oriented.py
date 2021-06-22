8ed529f8bce50cfd068e7d83278dbb002ea2424e
Elias <elias.rauth@hotmail.com>
2021-06-22 16:31:36 +0200

    adding clients to his own objects won't work

--- server_object-oriented.py
+++ server_object-oriented.py
@@ -11,10 +11,10 @@ import sys
 import threading
 
 codeset = 'cp850'  # or 'Latin-1' or 'UTF-8'
-clients = 0
+
 
 # Client-class
-class Client(object):
+class Clients(object):
     def __init__(self):
         self.connection, self.client_address = s.accept()
 
@@ -22,26 +22,57 @@ class Client(object):
         self.connection.sendall(("Gesendet: %s" % message).encode(codeset))
 
     def get_message(self):
-        pass
+        while True:
+            byte_data = self.connection.recv(300)
+            data = byte_data.decode(codeset)
+            print('received "%s"' % data, file=sys.stderr)
+            print('sending data back to the client', file=sys.stderr)
+            self.connection.sendall(("server got data: %s" % data).encode(codeset))
 
 
+'''
 s = socket.socket()  # Create a socket object
 host = ''  # unspecified ip - all interfaces on host
 port = 64001  # Reserve a port for your service.
 s.bind((host, port))  # Bind to the port
 
 s.listen(1)  # Now wait for client connection.
+new_clients = []
+clients = 1
+'''
+
+
+def get_new_clients():
+    client = Clients()
+    t1 = threading.Thread(target=client.send_message("hallo\n"))
+    t1.start()
+    t2 = threading.Thread(target=client.get_message())
+    t2.start()
+
+
+s = socket.socket()  # Create a socket object
+host = ''  # unspecified ip - all interfaces on host
+port = 64001  # Reserve a port for your service.
+s.bind((host, port))  # Bind to the port
+s.listen(1)  # Now wait for client connection.
+
+t0 = threading.Thread(target=get_new_clients())
+t0.start()
 
 while True:
-    new_client = Client()
+    pass
+    # client = Clients()
+    # print("Schleife")
     # new_client.send_message()
     # connection, client_address = s.accept()  # Establish / get one connection with client.
 #    handle_connection(connection,client_address)   # handle connections one by one
     # or start new thread for each connection
     # print('Got connection from %s' % str(client_address), file=sys.stderr)
 
-    t = threading.Thread(target=new_client.send_message("hallo"))
+    # t1 = threading.Thread(target=client.send_message("hallo\n"))
     # t = threading.Thread(target=handle_connection, args=(connection,))
-    t.start()
+    # t1.start()
+    # t2 = threading.Thread(target=client.get_message())
+    # t2.start()
     # and continue with loop accepting next connection
 # s.close()

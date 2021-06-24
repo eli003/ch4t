31c46bf4b5eba70b1385e69a06ded6ed31b7308d
Elias <elias.rauth@hotmail.com>
2021-06-24 20:20:49 +0200

    improving the code; still working on leaving client bug

--- server_object-oriented.py
+++ server_object-oriented.py
@@ -20,18 +20,21 @@ codeset = 'cp850'  # or 'Latin-1' or 'UTF-8'
 class Clients:
     client_list = list()    # All Clients shares the same client_list
 
-    def __init__(self, index):
+    def __init__(self, client_id):
         self.connection, self.client_address = s.accept()
-        type(self).client_list.append(self.connection)  # new clients get appended to the client_list
-        self.index = index
-        print("Connected Clients: ", len(type(self).client_list))
-        print(type(self).client_list)
+        self.client_list.append(self.connection)  # new clients get appended to the client_list
+        self.client_id = client_id
+        print("Connected Clients: ", len(self.client_list))
+        print(self.client_list)
 
-    def send_client_message_to_all(self, message):
+    def send_client_message_to_all(self, message, client_number):
         try:
-            for cnt, client in enumerate(type(self).client_list):  # goes through the list of clients
-                if cnt != self.index:  # if counter is not the same as the number of the sender: send
+            cnt = 1
+            print(client_number)
+            for client in self.client_list:  # goes through the list of clients
+                if client_number != cnt:  # if counter is not the same as the number of the sender: send
                     client.send(bytes(str(message), 'utf8'))  # send the message to another clients
+                cnt += 1
         finally:
             pass
 
@@ -47,20 +50,21 @@ class Clients:
                 full_msg = b''
                 byte_data = self.connection.recv(1024)   # incoming message
                 full_msg += byte_data
-                msg = (pickle.loads(full_msg[header_size:]))
-                # msg = byte_data.decode(codeset)   # decoding the message
-                print('received "%s" from Client %d' % (msg, self.index), file=sys.stderr)  # who send what message
-                self.send_client_message_to_all(msg)
+                # msg = (pickle.loads(full_msg[header_size:]))
+                msg = byte_data.decode(codeset)   # decoding the message
+                print('received "%s" from Client %d' % (msg, self.client_id), file=sys.stderr)  # who send what message
+                self.send_client_message_to_all(msg, self.client_id)  # sending unloaded pickle to other clients
         except(ConnectionAbortedError, ConnectionResetError):
-            print("Connection of Client", self.index, "lost!")
-            self.index -= 1
-            type(self).client_list.remove(self.connection)  # new clients get appended to the client_list
+            print("Connection of Client", self.client_id, "lost!")
+            self.client_list.remove(self.connection)  # leaving clients getting removed from client_list
+            print("Connected Clients: ", len(self.client_list))
+            print(self.client_list)
 
 
 def get_new_clients():
-    counter = 0  # for every new client, the counter increases
+    counter = 1  # for every new client, the counter increases
     while True:
-        client = Clients(counter)   # generating the object client of the class Clients
+        client = Clients(counter)  # generating the object client of the class Clients
         client.send_server_message_to_client('\nConnected to Server: %s:%s\n' % (local_ip, port))   # server sending msg
         client.send_server_message_to_client('\nYou are now connected to the ch4t-Server!\n'
                                              'Please be nice to other people ;)\n\n')
@@ -70,17 +74,18 @@ def get_new_clients():
         counter += 1  # counter increases
 
 
-s = socket.socket()  # Create a socket object
+if __name__ == '__main__':
+    s = socket.socket()  # Create a socket object
 
-hostname = socket.gethostbyname_ex(socket.gethostname())[-1]  # get the right ip-address of the server
-local_ip = hostname[1]
-# print(hostname)
-print("IP-address of the server: ", local_ip)
+    hostname = socket.gethostbyname_ex(socket.gethostname())[-1]  # get the right ip-address of the server
+    local_ip = hostname[1]
+    # print(hostname)
+    print("IP-address of the server: ", local_ip)
 
-host = local_ip  # unspecified ip - all interfaces on host
-port = 64001  # Reserve a port for your service.
-header_size = 10  # for pickle to load
-s.bind((host, port))  # Bind to the port
-s.listen(10)  # Now wait for client connection.
+    host = local_ip  # unspecified ip - all interfaces on host
+    port = 64001  # Reserve a port for your service.
+    header_size = 10  # for pickle to load
+    s.bind((host, port))  # Bind to the port
+    s.listen(10)  # Now wait for client connection.
 
-get_new_clients()    # function call for checking for new clients
+    get_new_clients()    # function call for checking for new clients
